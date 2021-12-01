f7a66febfd9f1d0338d88c1d28753be0fe55b5cc
Joe <soboleskijoe@gmail.com>
2021-11-30 21:24:24 -0800

    initial tool

---
+++ Editor/ToolsMenu.cs
@@ -0,0 +1,24 @@
+using UnityEditor;
+using UnityEngine;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static UnityEngine.Application;
+using static UnityEditor.AssetDatabase;
+
+public static class ToolsMenu {
+    
+    [MenuItem("Tools/Setup/Create Default Folders")]
+    public static void CreateDefaultFolders() {
+        CreateDirectories("", "Scripts", "Scenes", "Prefabs", "Materials", "Shaders", "Editor", "Plugins");
+
+        Refresh();
+    }
+
+    public static void CreateDirectories(string root, params string[] dir) {
+        string fullPath = Combine(dataPath, root);
+
+        foreach (string newDir in dir) {
+            CreateDirectory(Combine(fullPath, newDir));
+        }
+    }
+}
