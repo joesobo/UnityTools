94e9734d75abe3f22ebfc0105da9f37d8bb58f0d
Joe <soboleskijoe@gmail.com>
2021-12-04 16:19:25 -0800

    reinit again

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # UnityTools
 
 A collection of tools for my Unity Game Dev
+
+
+## Build
+
+To build run:
+
+```bash
+yarn build
+```
---
+++ build.sh
@@ -0,0 +1,5 @@
+# needs to copy the entire contents of the /Assets/ folder to the Tools dir
+
+#!/bin/bash
+
+cp -r ./Assets/* ../UnityPackages/Tools
