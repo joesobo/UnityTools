500e2e6201c331193d2c7f5c7d2f5b8bf5be32f2
Joe <soboleskijoe@gmail.com>
2021-12-04 16:44:39 -0800

    added new omnisharp option and refactor

--- Assets/Editor/Packages.cs
+++ Assets/Editor/Packages.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -5,16 +6,22 @@ using UnityEngine;
 
 public static class Packages {
 
-    public static async Task ReplacePackagesFromGist(string id, string user = "joesobo") {
-        var url = GetGistUrl(id, user);
+    public static async Task ReplacePackages(string user = "joesobo") {
+        var url = GetGistUrl("bb50095572f02a180a3a2653a07c5db0", user);
         var contents = await GetContents(url);
-        ReplacePackageFile(contents);
+        ReplaceFile(contents, "../Packages/manifest.json");
     }
 
-    public static async Task ReplaceGitIgnoreFromGist(string id, string user = "joesobo") {
-        var url = GetGistUrl(id, user);
+    public static async Task ReplaceGitIgnore(string user = "joesobo") {
+        var url = GetGistUrl("0d06e1d576df73a96a57f24b775afd0c", user);
         var contents = await GetContents(url);
-        ReplaceGitIgnoreFile(contents);
+        ReplaceFile(contents, "../.gitignore");
+    }
+
+    public static async Task ReplaceOmnisharp(string user = "joesobo") {
+        var url = GetGistUrl("149d7f062f32938a38b6e81c18d185ca", user);
+        var contents = await GetContents(url);
+        ReplaceFile(contents, "../omnisharp.json");
     }
 
     static string GetGistUrl(string id, string user = "joesobo") => $"https://gist.github.com/{user}/{id}/raw";
@@ -26,14 +33,8 @@ public static class Packages {
         return content;
     }
 
-    static void ReplacePackageFile(string content) {
-        var existing = Path.Combine(Application.dataPath, "../Packages/manifest.json");
-        File.WriteAllText(existing, content);
-        UnityEditor.PackageManager.Client.Resolve();
-    }
-
-    static void ReplaceGitIgnoreFile(string content) {
-        var existing = Path.Combine(Application.dataPath, "../.gitignore");
+    static void ReplaceFile(string content, string path) {
+        var existing = Path.Combine(Application.dataPath, path);
         File.WriteAllText(existing, content);
         UnityEditor.PackageManager.Client.Resolve();
     }
--- Assets/Editor/ToolsMenu.cs
+++ Assets/Editor/ToolsMenu.cs
@@ -4,17 +4,20 @@ using static Packages;
 
 public static class ToolsMenu {
 
-    [MenuItem("Tools/Setup/Create Default Folders")]
+    [MenuItem("Tools/Setup/Load/Create Default Folders")]
     static void CreateDefaultFolders() {
         Folders.CreateDirectories("", "Scripts", "Scenes", "Prefabs", "Materials", "Shaders", "Editor", "Plugins");
         Refresh();
     }
 
-    [MenuItem("Tools/Setup/Load New Manifest")]
-    static async void LoadNewManifest() => await ReplacePackagesFromGist("bb50095572f02a180a3a2653a07c5db0");
+    [MenuItem("Tools/Setup/Load/Load New Manifest")]
+    static async void LoadNewManifest() => await ReplacePackages();
 
-    [MenuItem("Tools/Setup/Load New GitIgnore")]
-    static async void LoadNewGitIgnore() => await ReplaceGitIgnoreFromGist("0d06e1d576df73a96a57f24b775afd0c");
+    [MenuItem("Tools/Setup/Load/Load New GitIgnore")]
+    static async void LoadNewGitIgnore() => await ReplaceGitIgnore();
+
+    [MenuItem("Tools/Setup/Load/Load Omnisharp")]
+    static async void LoadNewOmnisharp() => await ReplaceOmnisharp();
 
     [MenuItem("Tools/Setup/Packages/New Input System")]
     static void AddNewInputSystem() => InstallUnityPackage("inputsystem");
