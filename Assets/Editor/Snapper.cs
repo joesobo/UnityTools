8b0ae4ee8df605b7497cbd5b8d5f780a9eec6775
Joe <soboleskijoe@gmail.com>
2021-12-27 16:43:05 -0800

    feat: added polar coord converter

--- Assets/Editor/Snapper.cs
+++ Assets/Editor/Snapper.cs
@@ -4,17 +4,28 @@ using UnityEditor;
 using UnityEngine;
 
 public class Snapper : EditorWindow {
-    private int snapValue = 1;
+    private float snapValue = 1;
+    private float radiusSize = 1;
+    private float angleSize = 0;
+    private bool usePolarCoords = false;
 
     [MenuItem("Tools/Snapper")]
     public static void WindowSnapper() => GetWindow<Snapper>();
 
     private void OnGUI() {
         GUILayout.Label("Snapper", EditorStyles.boldLabel);
+        GUILayout.Space(20);
 
-        using (new GUILayout.HorizontalScope()) {
-            snapValue = EditorGUILayout.IntField("Snap to: ", snapValue);
+        usePolarCoords = GUILayout.Toggle(usePolarCoords, "Use Polar Coordinates");
+
+        GUILayout.Space(20);
+        if (usePolarCoords) {
+            radiusSize = EditorGUILayout.FloatField("Radius Size: ", radiusSize);
+            angleSize = EditorGUILayout.FloatField("Angle Size: ", angleSize);
+        } else {
+            snapValue = EditorGUILayout.FloatField("Snap to: ", snapValue);
         }
+
         if (GUILayout.Button("Snap Selection")) {
             Snap();
         }
@@ -23,7 +34,12 @@ public class Snapper : EditorWindow {
     private void Snap() {
         foreach (GameObject go in Selection.gameObjects) {
             Undo.RecordObject(go.transform, "Snap");
-            go.transform.position = go.transform.position.RoundToNearest(snapValue);
+
+            if (usePolarCoords) {
+                go.transform.position = go.transform.position.RoundToPolarCoords(radiusSize, angleSize);
+            } else {
+                go.transform.position = go.transform.position.RoundToNearest(snapValue);
+            }
         }
     }
 }
--- Assets/Editor/VectorLogic.cs
+++ Assets/Editor/VectorLogic.cs
@@ -8,14 +8,22 @@ public static class VectorLogic {
         return v;
     }
 
-    public static Vector3 RoundToNearest(this Vector3 v, int nearest) {
+    public static Vector3 RoundToNearest(this Vector3 v, float nearest) {
         v.x = RoundTo(v.x, nearest);
         v.y = RoundTo(v.y, nearest);
         v.z = RoundTo(v.z, nearest);
         return v;
     }
 
+    public static Vector3 RoundToPolarCoords(this Vector3 v, float radius, float angle) {
+        float newX = radius * Mathf.Cos(angle);
+        float newZ = radius * Mathf.Sin(angle);
+
+        return new Vector3(newX, 0, newZ);
+    }
+
     public static float RoundTo(float value, float multipleOf) {
         return Mathf.Round(value / multipleOf) * multipleOf;
     }
+
 }
